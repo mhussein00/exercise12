bc6727578bb1b2dee413871031cdbf10b513dc5b
mhussein00 <148893874+mhussein00@users.noreply.github.com>
2024-04-14 20:40:40 -0400

    Update Main.java

--- exercise12/src/Main.java
+++ exercise12/src/Main.java
@@ -1,3 +1,4 @@
+
 class QueueFullException extends Exception {}
 
 class QueueEmptyException extends Exception {}
@@ -7,19 +8,23 @@ class Queue<E> {
     private E[] elements;
     private int index = 0;
     private int size;
+    private int rear;
 
     Queue(int size) {
         elements = (E[]) new Object[size];
         this.size = size;
+        this.rear = rear;
     }
 
-    void enqueue(E element ) throws QueueFullException {
+    void enqueue(E element) throws QueueFullException {
         if (index >= size) {
             throw new QueueFullException();
         }
 
-        elements[index] = element;
-        index++;
+        elements[rear] = element;
+        rear++;
+        size++;
+
     }
 
     E dequeue () throws QueueEmptyException {
@@ -27,9 +32,12 @@ class Queue<E> {
             throw new QueueEmptyException();
         }
 
-        E returnElement = elements[index-2];
+        E returnElement = elements[index];
         index++;
         size--;
+
+
+
         return returnElement;
     }
 }
@@ -46,6 +54,7 @@ public class Main {
         } catch (QueueFullException | QueueEmptyException e) {
             e.printStackTrace();
         }
+
 
     }
 }
